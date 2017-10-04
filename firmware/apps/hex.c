05cc28163a947a7cfd8671d3adce260537f86391
Travis Goodspeed <travis@radiantmachines.com>
2017-10-04 12:45:14 -0400

    Hex viewer now blocks unknown peripheral at 0x1B00, allows reading the BSL.  Close #29.

--- firmware/apps/hex.c
+++ firmware/apps/hex.c
@@ -7,12 +7,30 @@
 //Include all standard GoodWatch functions.
 #include "api.h"
 
+//! Returns 1 if a given address is legal.
+static int hex_islegal(unsigned int adr){
+  return !(
+	   //(adr>0x1000 && adr<0x17ff) //Bootloader, can be unlocked.
+
+	   //Unknown peripheral; don't know how to unlock this region.
+	   (adr>=0x1b00 && adr<0x1c00)
+	   );
+}
+
 //! Entry to the hex editor app.
 void hex_init(){
   /* This function is called whenever the app is selected by the Mode
-     button, but we don't want to abandon our place, so we don't
-     actually initialize anything here.
-   */
+     button.  We don't initialize the address here, because we don't
+     want to have it reset, but we do disable some interrupts that
+     could trigger a crash on data reads.
+  */
+  
+  //disable interrupts for unmapped peripheral violations.
+  SFRIE1&=~(VMAIE|ACCVIE);
+  //Disable BSL protections, so we can read it without trouble.
+  SYSBSLC=3;
+
+  
   return;
 }
 
@@ -37,7 +55,8 @@ void hex_draw(){
      being drawn.
    */
   
-  static int adr=0x8000; //Beginning of Flash.
+  //static int adr=0x8000; //Beginning of Flash.
+  static int adr=0x1000; //Beginning of BSL.
   char ch=getchar();
 
   //Do nothing if no key is pressed.
@@ -76,8 +95,6 @@ void hex_draw(){
   }
 
   //And finally we draw the result to the screen.
-
-  
   lcd_zero();
 
   /* The bootloader range is illegal to read, so we skip it.  In the
@@ -85,19 +102,22 @@ void hex_draw(){
      the bootloader region, or trying to find the protection flag that
      is keeping us from accessing it.
    */
-  if(adr>0x1000 && adr<=0x17FF){
+  if(!hex_islegal(adr)){
     lcd_hex(
 	    (((unsigned long)adr)<<16) // Address
-	    | 0xdead //data
 	    );
+    lcd_cleardigit(3);
+    lcd_cleardigit(2);
+    lcd_cleardigit(1);
+    lcd_cleardigit(0);
     return;
+  }else{
+    /* Now that the range is legal and the value known, let's fetch the
+       value and display it.
+    */
+    lcd_hex(
+	    (((unsigned long)adr)<<16) // Address
+	    | ((unsigned int*)adr)[0] //data
+	    );
   }
-  
-  /* Now that the range is legal and the value known, let's fetch the
-     value and display it.
-   */
-  lcd_hex(
-	  (((unsigned long)adr)<<16) // Address
-	  | ((unsigned int*)adr)[0] //data
-	  );
 }
