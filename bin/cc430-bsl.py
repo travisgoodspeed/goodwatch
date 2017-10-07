fe10b6679488fa787c613552747b6166c241a2eb
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2017-10-06 20:20:25 -0700

    Add appropriate spaces after commas and around binary operators

--- bin/cc430-bsl.py
+++ bin/cc430-bsl.py
@@ -11,19 +11,19 @@ import serial, time, sys, argparse, string
 class BSL:
     def __init__(self, port):
         print("Opening %s" % port)
-        self.serial=serial.Serial(port,
+        self.serial = serial.Serial(port,
                                   baudrate=9600,
                                   parity=serial.PARITY_EVEN,
                                   stopbits=serial.STOPBITS_ONE,
                                   timeout=1)
 
     #Low level functions first.
-    def setTST(self,level):
+    def setTST(self, level):
         """Sets the TST pin."""
         self.serial.setRTS(level)
         time.sleep(0.01)
 
-    def setRST(self,level):
+    def setRST(self, level):
         """Sets the !RST pin."""
         self.serial.setDTR(level)
         time.sleep(0.01)
@@ -53,43 +53,43 @@ class BSL:
         self.setRST(False)
         self.setRST(True)
 
-    def crc(self,msg):
+    def crc(self, msg):
         """Returns a two-byte string of the checksum of a message."""
-        crc=0xFFFF
+        crc = 0xFFFF
         
         #msg should already include header bytes.
         for char in msg:
-            byte=ord(char)
-            x = ((crc>>8)^byte)&0xFF
-            x ^= x>>4
-            crc = (crc<<8)^(x<<12)^(x<<5)^x
-        return chr(crc&0xFF)+""+chr((crc>>8)&0xFF)
+            byte = ord(char)
+            x = ((crc >> 8) ^ byte) & 0xFF
+            x ^= x >> 4
+            crc = (crc << 8) ^ (x << 12) ^ (x << 5) ^ x
+        return chr(crc & 0xFF) + "" + chr((crc >> 8) & 0xFF)
 
-    def transact(self,msg):
+    def transact(self, msg):
         """Sends a message, wrapped with a prefix and checksum.
         Result's wrapper is stripped."""
 
         #Send the message.
-        length=len(msg)
-        ll=chr(length&0xFF)
-        lh=chr((length>>8)&0xFF)
-        crc=self.crc(msg)
-        self.serial.write("\x80"+ll+lh+msg+crc)
+        length = len(msg)
+        ll = chr(length & 0xFF)
+        lh = chr((length >> 8) & 0xFF)
+        crc = self.crc(msg)
+        self.serial.write("\x80" + ll + lh + msg + crc)
 
         #Get the reply.
-        reply=self.serial.read(1)
-        if len(reply)!=1:
+        reply = self.serial.read(1)
+        if len(reply) != 1:
             print "Error, missing reply."
             sys.exit(1)
-        elif ord(reply[0])==0x00:
+        elif ord(reply[0]) == 0x00:
             #Success
-            eighty=ord(self.serial.read(1))
-            ll=ord(self.serial.read(1)[0])
-            lh=ord(self.serial.read(1)[0])
-            length=ll|(lh<<8)
-            rep=self.serial.read(length)
-            crc=self.serial.read(2)
-            assert(crc==self.crc(rep))
+            eighty = ord(self.serial.read(1))
+            ll = ord(self.serial.read(1)[0])
+            lh = ord(self.serial.read(1)[0])
+            length = ll | (lh << 8)
+            rep  =  self.serial.read(length)
+            crc = self.serial.read(2)
+            assert(crc == self.crc(rep))
             return rep
         else:
             print "Error 0x%02x." % ord(reply[0])
@@ -100,99 +100,99 @@ class BSL:
         """Unlocks the bootloader, optionally with a password."""
 
         #Password must be 32 bytes.
-        assert(len(password)==32)
+        assert(len(password) == 32)
 
-        resp=self.transact("\x11"+password)
-        assert(len(resp)==2)
-        assert(resp[0]=='\x3b')
+        resp = self.transact("\x11" + password)
+        assert(len(resp) == 2)
+        assert(resp[0] == '\x3b')
 
-        code=ord(resp[1])
+        code = ord(resp[1])
     
-        if code==0x05:
+        if code == 0x05:
             print "Incorrect password.  Flash was erased."
             return self.unlock()
-        elif code==0x00:
+        elif code == 0x00:
             #success
             return True
         else:
-            print "Unexpected password core message 0x%02x."%code
+            print "Unexpected password core message 0x%02x." % code
             return False
         
     def version(self):
         """Gets the BSL version and related bytes."""
-        resp=self.transact("\x19")
-        assert(len(resp)==5)
+        resp = self.transact("\x19")
+        assert(len(resp) == 5)
         
-        vendor=ord(resp[1]) # 0 for TI
-        interpreter=ord(resp[2])
-        api=ord(resp[3]) # 0 for flash, 30 for sram, 80 for restricted cmd set
-        peripheral=ord(resp[4])
+        vendor = ord(resp[1]) # 0 for TI
+        interpreter = ord(resp[2])
+        api = ord(resp[3]) # 0 for flash, 30 for sram, 80 for restricted cmd set
+        peripheral = ord(resp[4])
         return vendor, interpreter, api, peripheral
 
     def masserase(self):
         """Bulk erases the device."""
-        resp=self.transact("\x15")
-        assert resp=="\x3b\x00"
+        resp = self.transact("\x15")
+        assert resp == "\x3b\x00"
 
-    def read(self,adr,length=32):
+    def read(self, adr, length=32):
         """Dumps memory from the given address."""
-        al=chr(adr&0xFF)
-        am=chr((adr>>8)&0xFF)
-        ah=chr((adr>>16)&0xFF)
-        ll=chr(length&0xFF)
-        lh=chr((length>>8)&0xFF)
-        resp=self.transact("\x18"+al+am+ah+ll+lh)
-        if resp[0]=="\x3b":
+        al = chr(adr & 0xFF)
+        am = chr((adr >> 8) & 0xFF)
+        ah = chr((adr >> 16) & 0xFF)
+        ll = chr(length & 0xFF)
+        lh = chr((length >> 8) & 0xFF)
+        resp = self.transact("\x18" + al + am + ah + ll + lh)
+        if resp[0] == "\x3b":
             print "Error: You need to unlock before reading."
-        assert(resp[0]=="\x3a")
+        assert(resp[0] == "\x3a")
         return resp[1:]
 
-    def write(self,adr,data=""):
+    def write(self, adr, data=""):
         """Writes memory to the given address."""
-        al=chr(adr&0xFF)
-        am=chr((adr>>8)&0xFF)
-        ah=chr((adr>>16)&0xFF)
-        resp=self.transact("\x10"+al+am+ah+data)
+        al = chr(adr & 0xFF)
+        am = chr((adr >> 8) & 0xFF)
+        ah = chr((adr >> 16) & 0xFF)
+        resp = self.transact("\x10" + al + am + ah + data)
         return resp[1:]
 
     
-    MAXLEN=256 #Maximum bytes per read request.
-    def readbulk(self,adr,length):
+    MAXLEN = 256 #Maximum bytes per read request.
+    def readbulk(self, adr, length):
         """Reads a large volume from the target, in multiple transactions."""
-        i=adr
-        buf=""
-        while i<adr+length:
-            buf=buf+self.read(i,min(self.MAXLEN,adr+length-i))
-            i=adr+len(buf)
-        assert(len(buf)==length)
+        i = adr
+        buf = ""
+        while i < adr + length:
+            buf = buf + self.read(i, min(self.MAXLEN, adr + length - i))
+            i = adr + len(buf)
+        assert(len(buf) == length)
         return buf
 
-    def writebulk(self,adr,data):
+    def writebulk(self, adr, data):
         """Writes a large volume to the target, in multiple transactions."""
-        length=len(data)
-        i=adr
-        while i<adr+length:
-            thislength=min(self.MAXLEN,adr+length-i)
-            self.write(i,data[i-adr:i-adr+thislength])
-            i=i+thislength
+        length = len(data)
+        i = adr
+        while i < adr + length:
+            thislength = min(self.MAXLEN, adr + length - i)
+            self.write(i, data[i - adr:i - adr + thislength])
+            i = i + thislength
 
-        #assert(i==adr+length)
+        #assert(i == adr + length)
         return True
 
-    def writeihexline(self,line):
+    def writeihexline(self, line):
         """Writes one line of an Intel Hex file.  (Langsec sin!)"""
-        assert(line[0]==':')
-        length=int(line[1:3],16)
-        adr=int(line[3:7],16)
-        verb=int(line[7:9],16)
+        assert(line[0] == ':')
+        length = int(line[1:3], 16)
+        adr = int(line[3:7], 16)
+        verb = int(line[7:9], 16)
         
-        data=line[9:(9+length*2)].decode('hex')
-        if verb==0: # Data
-            self.write(adr,data)
+        data = line[9:(9 + length * 2)].decode('hex')
+        if verb == 0: # Data
+            self.write(adr, data)
 
-    def writeihexfile(self,filename):
+    def writeihexfile(self, filename):
         """Writes an Intel Hex file to the CC430."""
-        f=open(filename,'r')
+        f = open(filename, 'r')
         for l in f:
             self.writeihexline(l.strip())
 
@@ -202,23 +202,23 @@ def coredump(bsl):
     print "\n\n\nDumping most of memory as a read test."
     
     ##Dump the BSL
-    bulk=bsl.readbulk(0x1000,2048)
+    bulk = bsl.readbulk(0x1000, 2048)
     print "Got %d bytes of the BSL." % len(bulk)
     print bulk.encode('hex')
 
     ##Dump Info
-    bulk=bsl.readbulk(0x1800,512)
+    bulk = bsl.readbulk(0x1800, 512)
     print "Got %d bytes of Info Flash." % len(bulk)
     print bulk.encode('hex')
 
     ##Dump RAM
-    bulk=bsl.readbulk(0x1C00,4096)
+    bulk = bsl.readbulk(0x1C00, 4096)
     print "Got %d bytes of RAM." % len(bulk)
     print bulk.encode('hex')
 
     ##Dump ROM
     print "Dumping Flash ROM.  Please be patient."
-    bulk=bsl.readbulk(0x8000,32*1024)
+    bulk = bsl.readbulk(0x8000, 32 * 1024)
     print "Got %d bytes of Flash ROM." % len(bulk)
     print bulk.encode('hex')
 
@@ -228,12 +228,12 @@ def writetest(bsl):
     print "\n\n\nMass erasing, then performing a write test."
     bsl.masserase()
     
-    msg="Hello world!  Hogy vagy?  Jo vagyok!"*35
-    bsl.writebulk(0x8000,msg)
-    readmsg=bsl.readbulk(0x8000,len(msg))
+    msg = "Hello world!  Hogy vagy?  Jo vagyok!" * 35
+    bsl.writebulk(0x8000, msg)
+    readmsg = bsl.readbulk(0x8000,len(msg))
     print "Wrote: %s" % msg.encode('hex')
     print "Read:  %s" % readmsg.encode('hex')
-    assert(readmsg==msg)
+    assert(readmsg == msg)
 
 
 # BSLException, Segment, and Memory are from goodfet.bsl
@@ -242,7 +242,7 @@ class BSLException(Exception):
 
 class Segment:
     """store a string with memory contents along with its startaddress"""
-    def __init__(self, startaddress = 0, data=None):
+    def __init__(self, startaddress=0, data=None):
         if data is None:
             self.data = ''
         else:
@@ -256,7 +256,7 @@ class Segment:
         return len(self.data)
 
     def __repr__(self):
-        return "Segment(startaddress = 0x%04x, data=%r)" % (self.startaddress, self.data)
+        return "Segment(startaddress=0x%04x, data=%r)" % (self.startaddress, self.data)
 
 class Memory:
     """represent memory contents. with functions to load files"""
@@ -284,25 +284,25 @@ class Memory:
         for l in lines:
             if l[0] != ':': raise BSLException("File Format Error\n")
             l = l.strip()       #fix CR-LF issues...
-            length  = int(l[1:3],16)
-            address = int(l[3:7],16)
-            type    = int(l[7:9],16)
-            check   = int(l[-2:],16)
+            length  = int(l[1:3], 16)
+            address = int(l[3:7], 16)
+            type    = int(l[7:9], 16)
+            check   = int(l[-2:], 16)
             if type == 0x00:
                 if currentAddr != address:
                     if segmentdata:
-                        self.segments.append( Segment(startAddr, string.join(segmentdata,'')) )
+                        self.segments.append(Segment(startAddr, string.join(segmentdata, '')))
                     startAddr = currentAddr = address
                     segmentdata = []
                 for i in range(length):
-                    segmentdata.append( chr(int(l[9+2*i:11+2*i],16)) )
+                    segmentdata.append(chr(int(l[9 + 2 * i:11 + 2 * i], 16)))
                 currentAddr = length + currentAddr
             elif type in (0x01, 0x02, 0x03, 0x04, 0x05):
                 pass
             else:
                 sys.stderr.write("Ignored unknown field (type 0x%02x) in ihex file.\n" % type)
         if segmentdata:
-            self.segments.append( Segment(startAddr, string.join(segmentdata,'')) )
+            self.segments.append(Segment(startAddr, string.join(segmentdata, '')))
 
     def loadTIText(self, file):
         """load data from a (opened) file in TI-Text format"""
@@ -319,14 +319,14 @@ class Memory:
             elif l[0] == '@':        #if @ => new address => send frame and set new addr.
                 #create a new segment
                 if segmentdata:
-                    self.segments.append( Segment(startAddr, string.join(segmentdata,'')) )
-                startAddr = int(l[1:],16)
+                    self.segments.append(Segment(startAddr, string.join(segmentdata, '')))
+                startAddr = int(l[1:], 16)
                 segmentdata = []
             else:
                 for i in string.split(l):
                     segmentdata.append(chr(int(i,16)))
         if segmentdata:
-            self.segments.append( Segment(startAddr, string.join(segmentdata,'')) )
+            self.segments.append(Segment(startAddr, string.join(segmentdata, '')))
 
     def loadELF(self, file):
         """load data from a (opened) file in ELF object format.
@@ -340,7 +340,7 @@ class Memory:
             if DEBUG:
                 sys.stderr.write("ELF section %s at 0x%04x %d bytes\n" % (section.name, section.lma, len(section.data)))
             if len(section.data):
-                self.segments.append( Segment(section.lma, section.data) )
+                self.segments.append(Segment(section.lma, section.data))
 
     def loadString(self, startAddr=0, string=None):
         """fill memory with the contents of a binary chunk of data."""
@@ -387,15 +387,15 @@ class Memory:
         return res
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='CC430F6137 BSL Client')
-    parser.add_argument('-e','--erase', help='Mass Erase', action='store_true')
-    parser.add_argument('-p','--port', help='Serial Port', default='/dev/ttyUSB0')
-    parser.add_argument('-f','--file', help='Flash File')
-    parser.add_argument('-P','--password', help='Password File or Hex')
-    parser.add_argument('-d','--dump', help='Produce a core dump.', action='store_true')
-    parser.add_argument('-u','--unlock', help='Unlock BSL.', action='store_true')
-    parser.add_argument('-t','--time', help='Set the Time.', action='store_true')
+    parser.add_argument('-e', '--erase', help='Mass Erase', action='store_true')
+    parser.add_argument('-p', '--port', help='Serial Port', default='/dev/ttyUSB0')
+    parser.add_argument('-f', '--file', help='Flash File')
+    parser.add_argument('-P', '--password', help='Password File or Hex')
+    parser.add_argument('-d', '--dump', help='Produce a core dump.', action='store_true')
+    parser.add_argument('-u', '--unlock', help='Unlock BSL.', action='store_true')
+    parser.add_argument('-t', '--time', help='Set the Time.', action='store_true')
     
     args = parser.parse_args()
 
@@ -429,9 +429,9 @@ if __name__=='__main__':
         print "Writing %s as Intel hex." % args.file
         bsl.writeihexfile(args.file)
     if args.time:
-        lt=time.localtime()
+        lt = time.localtime()
         #See firmware/rtc.c for the format.
-        timestr=(
+        timestr = (
             #Hour, Minute, Second first.
             chr(lt.tm_hour)+chr(lt.tm_min)+chr(lt.tm_sec)+"\xFF"+
             #u16 Year, u8 Month, u8 Day
