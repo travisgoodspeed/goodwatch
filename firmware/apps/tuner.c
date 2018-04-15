38491c9615e207531ea8af7ef8d2552b7238d984
Travis Goodspeed <travis@radiantmachines.com>
2018-04-15 17:41:13 -0400

    Tuner app now displays RSSI at all times.  Smoother for other features.  Toward #51

--- firmware/apps/tuner.c
+++ firmware/apps/tuner.c
@@ -7,6 +7,7 @@
   + and - change the channel, / shows the current working frequency,
   and 7 measures the signal strength on the current channel.
   
+  The = button can be used to set a VFO frequency.
 */
 
 #include<stdio.h>
@@ -14,6 +15,10 @@
 #include<msp430.h>
 #include "api.h"
 
+//Draws the codeplug name.
+static void draw(){
+  lcd_string(codeplug_name());
+}
 
 //! Enter the tuner tool.
 void tuner_init(){
@@ -45,13 +50,15 @@ int tuner_exit(){
 }
 
 //! Last character pressed in the tuner.
-static char lastchar;
+static int vfosetmode=0;
+static int rssi=0x5;
 
 //! Tuner keypress callback.
 int tuner_keypress(char ch){
-  lastchar=ch;
-
   switch(ch){
+  default:
+    draw();
+    break;
   case '+':  //Next channel.
     codeplug_next();
     codeplug_setfreq();
@@ -60,57 +67,65 @@ int tuner_keypress(char ch){
     codeplug_prev();
     codeplug_setfreq();
     break;
+  case '/':  //Show the frequency.
+    lcd_number(codeplug_getfreq()/10);
+    //setperiod(5,1);  //Periods are controlled by RSSI right now.
+    //setperiod(2,1);
+    break;
+  case '=':  //Set a frequency.  Not yet working.
+    vfosetmode=!vfosetmode;
+    break;
+  case '7':  //Show snapshot of scalar RSSI.
+    lcd_number(rssi);
+    lcd_string("RSSI");
+    break;
   }
 
   return 1;//Redraw.
 }
 
+void clearperiods(){
+  int i;
+  for(i=0; i<8; i++)
+    setperiod(i,0);
+}
+
 //! Draw the screen and increase the count.
 void tuner_draw(){
   static int i=0;
-  static int rssi=0x5;
   
-  switch(lastchar){
-  default: //Show the channel name
-    lcd_string(codeplug_name());
-    break;
-
-  case '/': //Show the frequency.
-    lcd_number(codeplug_getfreq()/10);
-    setperiod(5,1);
+  /* Every other frame, we grab the signal strength.  Highest
+     stays. */
+  if(!(i&1))
+    rssi=radio_getrssi();
+  
+  //Draw the new strength.
+  clearperiods();
+  switch(rssi&0xF0){
+  case 0xF0:
+  case 0xE0:
+    setperiod(0,1);
+  case 0xD0:
+    setperiod(1,1);
+  case 0xC0:
+  case 0xB0:
     setperiod(2,1);
-    break;
-  case '7': //Show scalar RSSI.
-    //Update the RSSI if appropriate.
-    if((i++&0x04)==4){
-      rssi=radio_getrssi();
-    }
-  
-    lcd_number(rssi);
-    lcd_string("RSSI");
-    
-    switch(rssi&0xF0){
-    case 0xF0:
-    case 0xE0:
-    case 0xD0:
-      setperiod(0,1);
-    case 0xC0:
-      setperiod(1,1);
-    case 0xB0:
-      setperiod(2,1);
-    case 0xA0:
-      setperiod(3,1);
-    case 0x90:
-      setperiod(4,1);
-    case 0x80:
-      setperiod(5,1);
-    case 0x70:
-      setperiod(6,1);
-    case 0x60:
-      setperiod(7,1);
-    }
-    break;
+  case 0xA0:
+  case 0x90:
+    setperiod(3,1);
+  case 0x80:
+  case 0x70:
+    setperiod(4,1);
+  case 0x60:
+  case 0x50:
+    setperiod(5,1);
+  case 0x40:
+  case 0x30:
+    setperiod(6,1);
+  case 0x20:
+    setperiod(7,1);
   }
-
+  
 }
+
 
