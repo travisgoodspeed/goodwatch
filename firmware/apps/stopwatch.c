7df06f49ad00077caed140d98a98aa169fcc78ab
Travis Goodspeed <travis@radiantmachines.com>
2018-11-15 17:03:06 -0500

    Cleanup of the stopwatch, eliminating expensive divisions.

--- firmware/apps/stopwatch.c
+++ firmware/apps/stopwatch.c
@@ -26,14 +26,20 @@ static int counting=0;
    To work around this, we maintain the count of hours separately from
    the general second count.
  */
-static unsigned int count, hour;
+static uint16_t count, hour;
+static uint8_t hourhex;
+static uint8_t min, minhex;
+static uint8_t sec, sechex;
 
+
+//! Renders the low bits of the count in quarter seconds.
+static const char subs[]={0, 0x25, 0x50, 0x75};
 
 //! Entry to the stopwatch app.
 void stopwatch_init(){
   //Zero the count.
-  count=0;
-  hour=0;
+  count=hour=min=sec=0;
+  
   //Start off without counting.
   counting=0;
 
@@ -62,9 +68,8 @@ int stopwatch_keypress(char ch){
       counting=!counting;
       break;
     case '0':
-      //Clear both the second counter and the hour counter.
-      count=0;
-      hour=0;
+      //Zero the count.
+      count=hour=min=sec=0;
       break;
   }
   
@@ -73,15 +78,10 @@ int stopwatch_keypress(char ch){
   return !counting;
 }
 
-//! Renders the low bits of the count in quarter seconds.
-static const char subs[]={0, 0x25, 0x50, 0x75};
-
 
 //! Draw the stopwatch app and handle its input.
 void stopwatch_draw(int forced){
-  unsigned int min, sec,
-    minhex, sechex, hourhex, subhex;
-
+  uint8_t subhex;
   
   /* The stopwatch is special in that it never times out.  Be very
      careful when doing this, because a minor bug might kill the
@@ -89,70 +89,74 @@ void stopwatch_draw(int forced){
    */
   app_cleartimer();
 
-  //If we aren't counting and there's not been a keypress, don't bother drawing.
+  //If we aren't counting and there's not been a keypress, don't
+  //bother drawing.
   if(!forced && !counting)
     return;
-
   
   //Increment the count if we're counting.
   if(counting)
     count++;
-  
-  //Divide the count.
-  min=((count>>2)/60)%60;
-  sec=(count>>2)%60;
+
+  //Update the subhex field.
   subhex=subs[count&3];
 
-  //Handle hour rollover.
-  if(((count>>2)/60)>60){
-    count=0;
+  //Handle the second rollover every forth tick.
+  if((count&3)==3){
+    sec++;
+    sechex=int2bcd(sec);
+  }
+
+  //Handle the minute rollover every 60 seconds.
+  if(sec>=60){
+    sec-=60;
+    sechex=int2bcd(sec);
+    min++;
+    minhex=int2bcd(min);
+  }
+  
+  //Handle hour rollover every 60 minutes
+  if(min>=60){
+    min-=60;
+    minhex=int2bcd(min);
     hour++;
     hourhex=int2bcd(hour);
   }
-
+  
+  
   //Blink the colon once a second.
   setcolon((count>>1)&1);
   
   //We either draw hhmmss or mmssSS.
-  if(hour){
-    //Only draw if the subseconds are zero.
-    if(!subhex){
-      sechex=int2bcd(sec);
-      lcd_digit(1,sechex>>4);
-      lcd_digit(0,sechex&0xF);
-
-      //Draw minutes if seconds are zero.
-      if(!sec){
-	minhex=int2bcd(min);
-	lcd_digit(4,minhex>>4);
-	lcd_digit(3,minhex&0xF);
-
-	//Draw hours if minutes are zero.
-	if(!min){
-	  hourhex=int2bcd(hour);
-	  lcd_digit(7,hourhex>>4);
-	  lcd_digit(6,hourhex&0xF);
-	}
-      }
+  if(hour && !subhex){
+    lcd_digit(1,sechex>>4);
+    lcd_digit(0,sechex&0xF);
+    
+    //Draw minutes and hours
+    if(!sec){
+      //Draw minutes
+      lcd_digit(4,minhex>>4);
+      lcd_digit(3,minhex&0xF);
+      
+      //Draw hours
+      lcd_digit(7,hourhex>>4);
+      lcd_digit(6,hourhex&0xF);
     }
-  }else{
+  }else if(!hour){
     //Draw the subsecond first.
     lcd_digit(1,subhex>>4);
     lcd_digit(0,subhex&0xF);
     
     //Only draw the rest if the subseconds have changed.
     if(!subhex || count==1){
-      sechex=int2bcd(sec);
       lcd_digit(4,sechex>>4);
       lcd_digit(3,sechex&0xF);
-
+      
       //Update minutes if the seconds are zero.
       if(!sec){
-	minhex=int2bcd(min);
 	lcd_digit(7,minhex>>4);
 	lcd_digit(6,minhex&0xF);
       }
     }
   }
-
 }
