d90bc9efe9ed1d27cdf4fbf7abd5617c43872949
Travis Goodspeed <travis@radiantmachines.com>
2019-11-05 22:25:38 -0500

    De Bruijn sequence from OpenSessame.  Needs ceilf() cut out.

---
+++ firmware/libs/bruijn.c
@@ -0,0 +1,201 @@
+/*! \file bruijn.c
+  \brief De Bruijn sequence generator.
+  
+  This short library is pretty much a direct port of an example by
+  Sammy Kamkar, which he published five years back for the IMME as
+  part of his OpenSesame project.  Like Sammy's example, much of this
+  is optimized for CC1101 radios driven by microcontrollers.
+  
+  Defining STANDALONE ought to let it build for desktops, and leaving
+  that undefined should make it suitable for use in firmware.
+  
+  --Travis
+*/
+
+#include<stdint.h>
+#include<string.h>
+#include<math.h>
+
+#include "bruijn.h"
+
+// we're making these global because we have limited stack and we're
+// calling some functions recursively
+uint8_t a[MAXBITS+1]; // largest bit size possible
+uint16_t s, k;
+uint8_t tmpi;
+uint8_t firstTx;
+
+// we could read from garages[] directly but this is faster
+uint8_t codelen;
+uint8_t bits, len;
+uint8_t tri;
+uint32_t b0, b1;
+//#define bits g.bits
+//#define len  g.len
+//#define tri  g.tri
+//#define b0   g.b0
+//#define b1   g.b1
+
+
+#ifdef LOCAL
+void rftx() { }
+void waitForTx() { }
+void printl(uint8_t line, char *str) { }
+int main() { db_send(); }
+uint8_t sequence[MAXLEN];
+#else
+uint8_t sequence[MAXLEN];
+#endif
+
+uint8_t realbuf[MAXLEN+1];
+
+
+// set or clear bit b in array A
+void setBit(uint8_t *A, uint16_t b, uint8_t val){
+  val ?
+    (A[b / 8] |=   1 << (b % 8)) :
+    (A[b / 8] &= ~(1 << (b % 8)));
+}
+
+// test bit b in array A
+uint8_t testBit(uint8_t *A, uint16_t b){
+  return ( ( A[b / 8] & (1 << (b % 8) ) ) != 0 );
+}
+
+// binary de bruijn sequence -samyk
+void db(uint8_t t, uint8_t p){
+  uint8_t j;
+  
+  if (t > bits){
+    if (bits % p == 0){
+      for (j = 1; j <= p; j++){
+	setBit(sequence, s++, a[j]);
+	
+	// once we have 254 bytes (may reduce because running out of
+	// space!), we need to tx due to size constraints
+	if ((s + 1) * len >= MAXLEN * 8)
+	  // transmit
+	  doTx();
+      }
+    }
+  }else{
+    a[t] = a[t-p];
+    
+    db(t+1, p);
+    for (j = a[t-p] + 1; j <= (tri ? 2 : 1); j++){
+      a[t] = j;
+      db(t+1, t);
+    }
+  }
+}
+
+// converts our bits to the actual bitstream/modulation the garage expects
+void convert_bits(){
+  uint8_t tlen;
+  uint16_t z;
+  uint32_t tb;
+
+  //FIXME: This should not be using floating point.
+  
+  // pull in last X bits of last transmission
+  if (!firstTx){
+    // let's keep last bits (10) * len (4) since we may have broken the true code up during this delay
+    for (tmpi = 0; tmpi < codelen; tmpi++){
+      //printf("s=%d cl=%d len=%d realbuf[%d] = rb[%d]\n", s, codelen, len, tmpi,
+      //			(int)(ceilf((s*len)/8.0)) - codelen + tmpi);
+      realbuf[tmpi] = realbuf[(int)(ceilf((s*len)/8.0)) - codelen + tmpi]; //FIXME What the fuck was this?
+      //realbuf[tmpi] = realbuf[((s*len)/8) - codelen + tmpi];
+      //printf("realbuf[%d]=%x\n", tmpi, realbuf[tmpi]);
+    }
+  }else
+    firstTx = 0;
+
+  // go through each bit and convert to the garage bitstream equivalent
+  // eg 0 = 1000, 1 = 1110
+  for (z = k / len; z < s; z++) {
+    tb = testBit(sequence, z) ? b1 : b0;
+
+    //Uncomment this to dump the sequence bits.
+    //printf("%d", tb==b1);
+    
+    tlen = len;
+    while (tlen--){
+      // there's gotta be a better way to do this but my brain hurts right now
+      setBit(realbuf, (7-(k%8))+((k/8)*8), (tb >> tlen) & 1);
+      k++;
+    }
+  }
+  
+  // start 10 bits in next time (assuming 10 bit code)
+  k = bits * len;
+  s = bits;
+}
+
+
+// start the de bruijn sequence using the recursive db() function
+void de_bruijn(){
+  // clear a[], s
+  for (tmpi = 0; tmpi <= MAXBITS; tmpi++)
+    a[tmpi] = 0;
+  s = 0;
+  firstTx = 1;
+  
+  // begin recursive de bruijn
+  db(1, 1);
+  
+  // a real de bruijn wraps, but since we can't wrap time we
+  // have to send the last of the bits to complete the sequence
+  for (tmpi = 0; tmpi < bits-1; tmpi++)
+    setBit(sequence, s++, 0);
+  
+  // transmit anything left over
+  doTx();
+}
+
+
+#ifdef STANDALONE
+
+#include<stdio.h>
+#include<assert.h>
+
+
+void doTx(){
+
+  //Wait for TX, which we can skip in simulation.
+
+  //Modify our buffer to the bits needed on the air.
+  convert_bits();
+
+  //Begin transmitting, which we can skip in simulation.
+
+}
+
+uint8_t _garage_id=0;
+
+//! Unix command-line tool for testing.
+int main(){
+  
+
+  //We'd first set power to maximum, except there's no radio in
+  //simulation.
+
+  //Then we'd set the frequency and CHANNR.
+
+  //And finally the fields of some garage door.
+  bits    = g.bits;
+  len     = g.len;
+  tri     = g.tri;
+  b0      = g.b0;
+  b1      = g.b1;
+  codelen = ceilf(bits * (len / 8.0));
+  
+  printf("\n");
+  
+  de_bruijn();
+
+  printf("\n");
+  
+  return 0; //All Good.
+}
+
+#endif
---
+++ firmware/libs/bruijn.h
@@ -0,0 +1,81 @@
+/*! \file bruijn.h
+  \brief Bruijn sequence header.
+
+*/
+
+
+#include <stdint.h>
+#include <math.h>
+
+
+// size should be 2**BITS+(BITS-1), but we're limited by chip
+// to 255 and further limited by stack size
+//#define MAXLEN 255 // maximum packet length
+#define MAXLEN 128 // maximum packet length
+#define MAXBITS 13
+
+#define G_HZ 0   // Hz
+#define G_BAUD 1 // baudrate
+#define G_BITS 2 // bits
+#define G_LEN 3  // each bit code is represented by multiple bits, so what is that length
+#define G_TRI 4  // is this trinary? (3-bit garages)
+#define G_0 5    // eg, if bit "0" is "1000", then 0x8 (1000)
+#define G_1 6    // bit 1's representation
+#define G_2 7    // only if trinary
+
+
+
+#define MHZ 26
+
+#ifdef LOCAL
+uint16_t PA_TABLE1, PA_TABLE0, FREQ0, FREQ1, FREQ2, CHANNR, MDMCFG3, MDMCFG4;
+uint8_t realbuf[MAXLEN+1];
+#else
+extern uint8_t realbuf[MAXLEN+1];
+#endif
+
+void rftx();
+void waitForTx();
+void db_send();
+void setBit(uint8_t *A, uint16_t b, uint8_t val);
+uint8_t testBit(uint8_t *A, uint16_t b);
+void db(uint8_t t, uint8_t p);
+void convert_bits();
+void de_bruijn();
+void setFreq();
+void setBaud();
+void doTx();
+
+
+
+
+const static struct {
+	uint32_t hz;
+	uint32_t baud;
+	uint8_t bits;
+	uint8_t len;
+	uint8_t tri;
+	uint32_t b0;
+	uint32_t b1;
+	uint32_t b2;
+} garages[] = {
+//        HZ, BAUD, BITS, LEN, TRI, B0,  B1,  B2
+
+	// Stanley, Linear Multicode & Moore-o-Matic, 8 & 10 dip switch, 310MHz
+	{310000000, 2000,   10,   4,   0, 0x8, 0xe, 0x0},
+
+	// Chamberlain, 7, 8 & 9 dip switch, 390MHz
+	{390000000, 2000,    9,   4,   0, 0x8, 0xe, 0x0},
+
+	// Chamberlain Canada, 9 dip switch, 315MHz
+	{315000000, 2000,    9,   4,   0, 0x8, 0xe, 0x0},
+
+	// NSCD is 2k baud rate but tri-state
+  // 0 = 100000000100000000, 1 = 111111110100000000, 2 = 111111110111111110
+	{318000000, 2000,    9,  18,   1, 0x020100, 0x03fd00, 0x03fdfe},
+
+};
+
+extern uint8_t _garage_id;
+#define g garages[_garage_id]
+
