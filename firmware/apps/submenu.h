17645b576ac30efd557e274d7144d880397ef315
Travis Goodspeed <travis@radiantmachines.com>
2021-09-09 16:08:50 -0400

    Adds a missing function declaration.

--- firmware/apps/submenu.h
+++ firmware/apps/submenu.h
@@ -9,6 +9,9 @@ void submenu_draw();
 //! Change the selected applet.
 int submenu_keypress(char c);
 
+//! Lets a keypress fall through from the clock to the select submenu applet.
+int submenu_fallthrough(char ch);
+
 //! On exit, set the submenu app.
 int submenu_exit();
 
